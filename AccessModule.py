aef74b723f7e2c160770dd902af2be1912c2ebe0
Adrian Almeida Garcia <72347171+Adrito981119@users.noreply.github.com>
2021-09-26 17:22:18 -0400

    Add files via upload

--- AccessModule.py
+++ AccessModule.py
@@ -9,14 +9,15 @@ bot = telegram.Bot(token = env_vars.get_my_token())
 
 #se usa para saber si el usuario pertenece al canal donde esta el bot
 def access(update: Update,id):
-    person = bot.get_chat_member(env_vars.get_channel().id).status
+    person = bot.get_chat_member(env_vars.get_channel(), id).status
+    print(bot.get_chat_member(env_vars.get_channel(), id).status)
     if person == 'member' or person == 'creator' or person == 'administrator' :
         return True
     else:
         return False
 
 def admin_access(update: Update, id):
-    admin = bot.get_chat_member(env_vars.get_channel().id).status
+    admin = bot.get_chat_member(env_vars.get_channel(), id).status
     if admin == 'creator' or admin == 'administrator' : return True
     else: 
         return False
